13b35542f8cfdc1769814f0b17b74f623f937789
ymnhn <r5c6tb8b6h@privaterelay.appleid.com>
2026-05-08 22:35:07 +0900

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  // IMPORTANT: Replace with your actual GitHub Pages URL
+  site: "https://ymnhn.github.io",
+  
+  // IMPORTANT: If your repo is named 'cognigraph', uncomment the line below.
+  // If your repo is named 'your-username.github.io', leave it commented out.
+  base: "/cognigraph",
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  scopedStyleStrategy: "where",
+});
