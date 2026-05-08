5002dcaaf00d284dada6b16a96c86961dd17c519
ymnhn <r5c6tb8b6h@privaterelay.appleid.com>
2026-05-09 01:35:20 +0900

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,23 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { SITE } from "./src/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
     sitemap(),
     mdx(),
     react(),
   ],
   markdown: {
-    remarkPlugins: [],
-    rehypePlugins: [],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
     },
   },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
-  scopedStyleStrategy: "where",
-  // 환경 변수 설정 (에러가 발생했던 부분)
-  env: {
-    schema: {
-      PUBLIC_GOOGLE_SITE_VERIFICATION: envField.string({
-        context: "client",
-        access: "public",
-        optional: true,
-      }),
-    },
-  },
+  // env 설정 부분을 과감히 삭제하거나 주석 처리하세요.
 });
