0665c97244efa97a6e85a912b4a3c0ce3c672428
ymnhn <r5c6tb8b6h@privaterelay.appleid.com>
2026-05-09 01:31:23 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,43 @@
-import { defineConfig, envField } from "astro/config"; // envField 추가
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import path from "path";
+import react from "@astrojs/react";
+import { SITE } from "./src/config";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://ymnhn.github.io",
-  base: "/cognigraph",
-  
-  // astro:env 모듈 에러 해결을 위한 스마 정의
-  experimental: {
-  env: {
-    schema: {
-      PUBLIC_GOOGLE_SITE_VERIFICATION: envField.string({
-        context: 'client',
-        access: 'public',
-        optional: true, // 값이 없어도 빌드가 실패하지 않도록 설정
-      }),
-    },
-  },
-});
+  site: SITE.website,
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
-    react(),
     sitemap(),
     mdx(),
+    react(),
   ],
-
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [remarkCollapse, { test: "Table of contents" }],
-    ],
+    remarkPlugins: [],
+    rehypePlugins: [],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
     },
   },
-
   vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve("./src"),
-      },
-    },
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
+    },
+  },
+  scopedStyleStrategy: "where",
+  // 환경 변수 설정 (에러가 발생했던 부분)
+  env: {
+    schema: {
+      PUBLIC_GOOGLE_SITE_VERIFICATION: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+      }),
     },
   },
 });
